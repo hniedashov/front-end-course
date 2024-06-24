ccdb1ba146001f72dad2c9d77027c53909beabe2
dmytro <dHniedashov@spinemedia.com>
2024-06-24 17:02:56 +0300

    finished task 4.3

--- homework/4/src/js/index.js
+++ homework/4/src/js/index.js
@@ -1,9 +1,11 @@
 import {PromptQuestionAnswer} from "./tasks/sub-task-1";
 import {NumberChecker} from "./tasks/sub-task-2";
+import {CustomerInfo} from "./tasks/sub-task-3";
 
 export const Homework_4 = () => {
     document.addEventListener('DOMContentLoaded', () => {
         PromptQuestionAnswer.init();
         NumberChecker.init();
+        CustomerInfo.init();
     });
 };
