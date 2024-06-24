be47c98c36188de388cc24b9a7a054f26bd31c89
dmytro <dHniedashov@spinemedia.com>
2024-06-24 15:07:07 +0300

    finished task 4.2

--- homework/4/src/js/index.js
+++ homework/4/src/js/index.js
@@ -1,7 +1,9 @@
 import {PromptQuestionAnswer} from "./tasks/sub-task-1";
+import {NumberChecker} from "./tasks/sub-task-2";
 
 export const Homework_4 = () => {
     document.addEventListener('DOMContentLoaded', () => {
         PromptQuestionAnswer.init();
+        NumberChecker.init();
     });
 };
